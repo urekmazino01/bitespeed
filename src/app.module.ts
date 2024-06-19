27bcaccd09b75720884ba8b3c6e6c0e2b1494388
Uttam <uttam@WILASSETA008.local>
2024-06-19 18:39:18 +0530

    feat: remove product

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
-import { ProductsModule } from './products/products.module';
 import { ContactsModule } from './contacts/contacts.module';
 import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [DatabaseModule, ProductsModule, ContactsModule, PrismaModule],
+  imports: [DatabaseModule, ContactsModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
