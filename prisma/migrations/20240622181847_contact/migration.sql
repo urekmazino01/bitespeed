39ee3bc76e04b8bfc4adeebe4a145cc2b72094ba
Your Name <you@example.com>
2024-06-22 23:53:25 +0530

    fix: merge

---
+++ prisma/migrations/20240622181847_contact/migration.sql
@@ -0,0 +1,16 @@
+-- CreateTable
+CREATE TABLE "Contact" (
+    "id" SERIAL NOT NULL,
+    "phoneNumber" TEXT NOT NULL,
+    "emailId" TEXT NOT NULL,
+    "linkedId" INTEGER,
+    "linPrecedence" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "deletedAt" TIMESTAMP(3),
+
+    CONSTRAINT "Contact_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Contact_phoneNumber_emailId_key" ON "Contact"("phoneNumber", "emailId");
