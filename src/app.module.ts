49c49789f5633ece5e3928b00a49c35791c96122
Your Name <you@example.com>
2024-06-22 23:21:33 +0530

    fix: merge

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+<<<<<<< HEAD
+=======
+import { DatabaseModule } from './database/database.module';
+import { ProductsModule } from './products/products.module';
+>>>>>>> 8cb2c44 (feat: contacts)
 import { ContactsModule } from './contacts/contacts.module';
 import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
+<<<<<<< HEAD
   imports: [ContactsModule, PrismaModule],
+=======
+  imports: [DatabaseModule, ProductsModule, ContactsModule, PrismaModule],
+>>>>>>> 8cb2c44 (feat: contacts)
   controllers: [AppController],
   providers: [AppService],
 })
--- src/contacts/contacts.service.ts
+++ src/contacts/contacts.service.ts
@@ -6,6 +6,7 @@ import { PrismaService } from 'src/prisma/prisma.service';
 @Injectable()
 export class ContactsService {
   constructor(private readonly prisma: PrismaService) {}
+<<<<<<< HEAD
  async create(createContactDto: CreateContactDto){
 
   const contacts = await this.prisma.contact.findMany({
@@ -256,4 +257,186 @@ export class ContactsService {
      },
     }
  }
+=======
+  async create(createContactDto: CreateContactDto) {
+    const count = (await this.prisma.contact.findMany()).length;
+    const primaryResult = await this.prisma.contact.findMany({
+      where: {
+        linPrecedence: 'primary',
+        OR: [
+          { emailId: createContactDto.email },
+          { phoneNumber: createContactDto.phoneNumber },
+        ],
+      },
+    });
+    const resultByPn = await this.prisma.contact.findMany({
+      where: {
+        phoneNumber: createContactDto.phoneNumber,
+      },
+    });
+
+    const resultByEm = await this.prisma.contact.findMany({
+      where: {
+        emailId: createContactDto.email,
+      },
+    });
+
+    if (resultByPn.length === 0 && resultByEm.length === 0) {
+      const result = await this.prisma.contact.create({
+        data: {
+          Id: count,
+          emailId: createContactDto.email,
+          phoneNumber: createContactDto.phoneNumber,
+          linkedId: null,
+          linPrecedence: 'primary',
+          deletedAt: null,
+        },
+      });
+
+      const allContacts = await this.prisma.contact.findMany({
+        where: {
+          OR: [
+            { emailId: createContactDto.email },
+            { phoneNumber: createContactDto.phoneNumber },
+          ],
+        },
+        orderBy: {
+          createdAt: 'asc',
+        },
+      });
+      let emails = [];
+      let phoneNumbers = [];
+      let secondaryContactIds = [];
+
+      for (let ele of allContacts) {
+        emails.push(ele.emailId);
+        phoneNumbers.push(ele.phoneNumber);
+
+        if (ele.linPrecedence !== 'primary') {
+          secondaryContactIds.push(ele.linkedId);
+        }
+      }
+
+      return {
+        contact: {
+          primaryContactId: resultByPn[0].id,
+          emails: emails,
+          phoneNumbers: phoneNumbers,
+          secondaryContactIds: secondaryContactIds,
+        },
+      };
+    } else {
+      if (
+        resultByPn.length === 1 &&
+        resultByEm.length === 1 &&
+        resultByPn[0].linPrecedence === 'primary' &&
+        resultByEm[0].linPrecedence === 'primary'
+      ) {
+        if (
+          resultByPn[0].linPrecedence === 'primary' &&
+          resultByEm[0].linPrecedence === 'primary'
+        ) {
+          await this.prisma.contact.update({
+            where: {
+              id: resultByPn[0].id,
+            },
+            data: {
+              linkedId: resultByEm[0].Id,
+              linPrecedence: 'secondary',
+            },
+          });
+
+          const allContacts = await this.prisma.contact.findMany({
+            where: {
+              OR: [
+                { emailId: createContactDto.email },
+                { phoneNumber: createContactDto.phoneNumber },
+              ],
+            },
+          });
+          let emails = [];
+          let phoneNumbers = [];
+          let secondaryContactIds = [];
+
+          for (let ele of allContacts) {
+            emails.push(ele.emailId);
+            phoneNumbers.push(ele.phoneNumber);
+
+            if (ele.linPrecedence !== 'primary') {
+              secondaryContactIds.push(ele.linkedId);
+            }
+          }
+
+          return {
+            contact: {
+              primaryContactId: resultByPn[0].id,
+              emails: emails,
+              phoneNumbers: phoneNumbers,
+              secondaryContactIds: secondaryContactIds,
+            },
+          };
+        }
+      }
+      const result = await this.prisma.contact.create({
+        data: {
+          Id: count,
+          emailId: createContactDto.email,
+          phoneNumber: createContactDto.phoneNumber,
+          linkedId: primaryResult[0].Id,
+          linPrecedence: 'secondary',
+          deletedAt: null,
+        },
+      });
+
+      const allContacts = await this.prisma.contact.findMany({
+        where: {
+          OR: [
+            { emailId: createContactDto.email },
+            { phoneNumber: createContactDto.phoneNumber },
+          ],
+        },
+        orderBy: {
+          createdAt: 'asc',
+        },
+      });
+      let emails = [];
+      let phoneNumbers = [];
+      let secondaryContactIds = [];
+
+      for (let ele of allContacts) {
+        emails.push(ele.emailId);
+        phoneNumbers.push(ele.phoneNumber);
+
+        if (ele.linPrecedence !== 'primary') {
+          secondaryContactIds.push(ele.linkedId);
+        }
+      }
+
+      return {
+        contact: {
+          primaryContactId: resultByPn[0].id,
+          emails: emails,
+          phoneNumbers: phoneNumbers,
+          secondaryContactIds: secondaryContactIds,
+        },
+      };
+    }
+  }
+
+  findAll() {
+    return `This action returns all contacts`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} contact`;
+  }
+
+  update(id: number, updateContactDto: UpdateContactDto) {
+    return `This action updates a #${id} contact`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} contact`;
+  }
+>>>>>>> 8cb2c44 (feat: contacts)
 }
