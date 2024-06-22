27da752f92f0dbefefbfaf6fd7cc315cf87ceab1
Your Name <you@example.com>
2024-06-22 23:24:21 +0530

    fix: master

--- src/contacts/contacts.controller.ts
+++ src/contacts/contacts.controller.ts
@@ -11,7 +11,6 @@ import { ContactsService } from './contacts.service';
 import { CreateContactDto } from './dto/create-contact.dto';
 import { UpdateContactDto } from './dto/update-contact.dto';
 
-<<<<<<< HEAD
 @Controller('identify')
 export class ContactsController {
   constructor(private readonly contactsService: ContactsService) {}
@@ -20,34 +19,4 @@ export class ContactsController {
   async create(@Body() createContactDto: CreateContactDto) {
     return await this.contactsService.create(createContactDto);
   }
-=======
-@Controller('contacts')
-export class ContactsController {
-  constructor(private readonly contactsService: ContactsService) {}
-
-  @Post()
-  async create(@Body() createContactDto: CreateContactDto) {
-    return await this.contactsService.create(createContactDto);
-  }
-
-  @Get()
-  findAll() {
-    return this.contactsService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.contactsService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateContactDto: UpdateContactDto) {
-    return this.contactsService.update(+id, updateContactDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.contactsService.remove(+id);
-  }
->>>>>>> 8cb2c44 (feat: contacts)
 }
